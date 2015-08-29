be5c4963ce4c5f44e1b2dc6f2eae001980dcd57b
Wei Gou <gouwei@live.com>
2015-08-30 02:24:15 +0800

    init

---
+++ ConsoleApplicationPATExercise/Main.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplicationPATExercise.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "DataStructure.h"
+#include "pat-basic.h"
+
+
+
+int main(void)
+{
+
+	test();
+
+
+	system("pause");
+	return 0;
+
+}
+
+
---
+++ ConsoleApplicationPATExercise/pat-basic.h
@@ -0,0 +1,4 @@
+#pragma once
+void test(void);
+void pat_basic_1004(void);
+
